555e5babc152cf60b78e363c9981f6e524557d3d
lexara-prime-ai <irfanghat@gmail.com>
2024-05-29 17:38:24 +0000

    Updated [RESPONSE] format.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 /*
+
+    [QUERY]
+    wget -q -O - "http://db1.wspr.live/?query=SELECT * FROM wspr.rx LIMIT 5 FORMAT JSON;"
+
    <result_format> [options]:
        JSON
        JSONCompact
@@ -12,12 +16,13 @@ use wspr::{services::prelude::*, state::prelude::*};
 
 #[tokio::main]
 async fn main() {
+    // Usage examples.
     let mut state = ClickHouseClient::init();
     let session = session_manager::SessionManager::new();
 
-    ClickHouseClient::dispatch(&mut state, ClickHouseAction::Get, "2", "JSON").await;
+    ClickHouseClient::dispatch(&mut state, ClickHouseAction::Get, "10", "JSON").await;
 
-    // println!("\n{:#?}\n", state);
+    println!("\n{:#?}\n", state);
 
     // ClickHouseClient::dispatch(&mut state, ClickHouseAction::GetById(1));
     // println!("\n[OUTPUT]: {:?}", state);
@@ -25,11 +30,3 @@ async fn main() {
     // let json_response = serde_json::to_string_pretty(&response).unwrap();
     // println!("{}", json_response);
 }
-
-/*
-
-   [QUERY]
-
-   wget -q -O - "http://db1.wspr.live/?query=SELECT * FROM wspr.rx LIMIT 5 FORMAT JSON;"
-
-*/
