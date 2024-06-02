5bb3e17bcd1393eb4aa9ab138ffe30a237a9328c
lexara-prime-ai <irfanghat@gmail.com>
2024-06-02 11:46:13 +0000

    Added documentation for python_wrapper module

--- python_wrapper/src/lib.rs
+++ python_wrapper/src/lib.rs
@@ -2,7 +2,7 @@
 use pyo3::{prelude::*, wrap_pyfunction};
 use wspr_cdk::{services::prelude::*, state::prelude::*};
 
-/// Wrap ClickHouseState in a new struct that implements IntoPy
+/// Wrap ClickHouseState in a new struct that implements IntoPy.
 #[pyclass]
 struct ClickHouseStateWrapper {
     inner: ClickHouseState,
@@ -17,7 +17,28 @@ impl ClickHouseStateWrapper {
         }
     }
 
-    // You can add methods to interact with ClickHouseState if needed
+    /*
+        >> The '__repr__'  method will provide a meaningful [string] representaion
+        for the ClickHouseState, python <object>.
+        _________________________________________________________________
+        >> The '__str__'  method will provide a user-friendly [string] representaion
+        for the ClickHouseState, python <object>.
+    */
+    fn __repr__(&self) -> String {
+        format!(
+            "ClickHouseStateWrapper with some internal state: {:?}",
+            self.inner
+        )
+    }
+
+    fn __str__(&self) -> String {
+        format!(
+            "ClickHouseStateWrapper with some internal state: {:?}",
+            self.inner
+        )
+    }
+
+    // [To Do] -> Add more methods to interact with ClickHouseState if needed.
 }
 
 #[pyfunction]
@@ -29,13 +50,13 @@ fn get_wspr_spots(py: Python, limit: String, result_format: String) -> PyResult<
         ClickHouseClient::dispatch(&mut state, ClickHouseAction::Get, &limit, &result_format).await;
 
         Ok(Python::with_gil(|py| {
-            // Convert the state into a Python object
+            // Convert the state into a Python <object>.
             Py::new(py, ClickHouseStateWrapper { inner: state }).unwrap()
         }))
     })
 }
 
-/// A Python module implemented in Rust.
+/// The <wspr_cdk> python module, implemented in Rust.
 #[pymodule]
 fn python_wrapper(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_wspr_spots, m)?)?;
