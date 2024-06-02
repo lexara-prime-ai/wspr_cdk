d0c22968d4d971460fd4b92bda074902137bdacf
lexara-prime-ai <irfanghat@gmail.com>
2024-06-02 08:46:13 +0000

    Added python wrapper

--- hyper/hyper/server.py
+++ hyper/hyper/server.py
@@ -1,15 +1,21 @@
-from typing import Union
+# from typing import Union
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-server = FastAPI()
-
-
-@server.get("/")
-def read_root():
-    return { "Allahu": "Akbar" }
+# server = FastAPI()
 
 
-@server.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @server.get("/")
+# def read_root():
+#     return { "Allahu": "Akbar" }
+
+
+# @server.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+import python_wrapper;
+
+output = python_wrapper.sum_as_string(6, 9)
+
+print('Output: ', output)
