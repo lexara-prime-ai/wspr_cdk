a643f2bd2bf32d95b6f5becad9a3fffcdf1758b5
lexara-prime-ai <irfanghat@gmail.com>
2024-06-06 22:35:33 +0000

    Added [To do] item.

--- scripts/bash/inspect_build.sh
+++ scripts/bash/inspect_build.sh
@@ -2,3 +2,6 @@
 
 printf "\n[Inspecting] <test> container...\n"
 sudo docker run -it test python ./hyper/hyper/server.py --interval 5
+
+# To do -> Add commands for inspecting resulting image
+# docker image inspect [OPTIONS] IMAGE [IMAGE...]
