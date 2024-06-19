b5506c808de1ea8717eec948195fb8c04b70a9ff
lexara-prime-ai <irfanghat@gmail.com>
2024-06-19 14:17:35 +0000

    Updated <error_handling> module

--- hyper/hyper/error_handling.py
+++ hyper/hyper/error_handling.py
@@ -3,6 +3,10 @@ CRED = "\033[91m"
 CEND = "\033[0m"
 
 
+ # -----------------------------------
+"""Custom <error> handling."""
+
+# -----------------------------------
 class ErrorHandling:
     def propagate_error(self, process_name: str, message: str):
         print(
